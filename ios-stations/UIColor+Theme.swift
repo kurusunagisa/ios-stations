be2006de00277cd7df9347a82efac8c5b56b41aa
Kurusu Nagisa <kurusunagisa963@gmail.com>
2022-02-25 16:31:21 +0900

    fix2

--- ios-stations/UIColor+Theme.swift
+++ ios-stations/UIColor+Theme.swift
@@ -15,10 +15,3 @@ extension UIColor {
         return UIColor(red: r, green: 1.0, blue: 1.0, alpha: 1.0)
     }
 }
-
-class ViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.view.backgroundColor = UIColor.Theme.main
-    }
-}
